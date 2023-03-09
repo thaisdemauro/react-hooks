b2940514200853f26948116e63134630ee40f00f
thais <thaisster@gmail.com>
2023-03-09 18:01:40 -0300

    react-hooks

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import './App.css';
+
+import Usuarios from './components/Usuarios/Usuarios';
+
+function App () {
+  return (
+    <div className="App">
+      <header>
+        <nav>
+          <ul>
+            <li><a href="/">Inicio</a></li>
+          </ul>
+        </nav>
+      </header>
+
+
+
+
+    </div>
+  )
+}
+
+export default App 
